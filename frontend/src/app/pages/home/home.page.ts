1479ec11cc5de5278ff26dcdecb3147737473693
Pedro Jorquera <pjorquera@okode.com>
2019-08-19 01:32:25 +0200

    Added UI

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Item } from '../../models/item';
+import { ItemsService } from '../../services/items.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  readonly items: Item[] = [];
+
+  constructor(
+    private alertCtrl: AlertController,
+    private itemsService: ItemsService
+    ) {}
+
+  async add() {
+    const alert = await this.alertCtrl.create({
+      header: 'New item',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+        {
+          name: 'description',
+          placeholder: 'Description'
+        }],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: (item: Item) => { this.itemsService.add(item); }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  edit(item: Item) {
+    console.log('Edit not implemented');
+  }
+
+  remove(item: Item) {
+    console.log('Delete not implemented');
+  }
 
 }
